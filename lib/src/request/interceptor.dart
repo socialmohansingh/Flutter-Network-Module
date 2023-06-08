93c862ae052b952cded8878a7618b7129efba341
Mohan Singh Thagunna <socialmohansingh@gmail.com>
2023-06-08 10:33:55 +0545

    Update interceptor.dart

--- lib/src/request/interceptor.dart
+++ lib/src/request/interceptor.dart
@@ -3,7 +3,7 @@ import 'package:flutter_netwok_module/src/network/network_failure.dart';
 import 'package:flutter_netwok_module/src/request/request_api.dart';
 import 'package:flutter_netwok_module/src/network/network_client.dart';
 
-abstract class Interceptor {
+abstract class AppInterceptor {
   Future<Result<NetworkFailure, RequestApi>> onRequest(
     RequestApi api,
     NetworkClient client,
